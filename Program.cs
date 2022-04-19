0b037959838fe1250b4ae4b86db5c81e8145f333
ramazantufekci <ramazan.tufekci@outlook.com>
2022-04-19 16:36:54 +0300

    değişkenler

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı yazınız:");
+            string soyad = Console.ReadLine();
+            Console.WriteLine(name+" "+soyad);
+        }
+    }
+}
